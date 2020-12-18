5cc86e97048fcd7e79a88740ed59df2ad5b6855d
owncreed93 <christiantarifer@gmail.com>
2020-12-17 23:44:46 -0500

    Added component styles to the form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+
+// **************************************************************************** //
+
+import styled from '@emotion/styled';
+
+// **************************************************************************** //
+
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+// **************************************************************************** //
+
+const Contenedor = styled.div`
+
+      max-width: 600px;
+      padding: 0 3rem;
+      margin: 0 auto;
+
+`;
+
+const ContenedorFormulario = styled.div`
+
+      background-color: #FFF;
+      padding: 3rem;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Contenedor>
+
+        <Header titulo='Cotizador de Seguros' />
+
+        <ContenedorFormulario>
+          <Formulario />
+        </ContenedorFormulario>
+
+      </Contenedor>
+
+
+    // https://gist.github.com/juanpablogdl/c70d8f047c3d8c99abfbb1c92f6f4e5e
   );
 }
 
